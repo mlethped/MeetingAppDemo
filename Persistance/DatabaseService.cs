f67f1100f27b6abc27d22e4d0993cb2d7c8d76b4
Martin Pedersen <mlethped@gmail.com>
2020-03-03 17:29:05 +0100

    Added basic persistance layer

---
+++ Persistance/DatabaseService.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interfaces;
+using Domain.MeetingRooms;
+using Persistance.MeetingRooms;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace Persistance
+{
+    public class DatabaseService : DbContext, IDatabaseService
+    {
+        public IDbSet<MeetingRoom> MeetingRooms { get; set; }
+
+        public DatabaseService() 
+            : base("MeetingApp")
+        {
+        }
+
+        public void Save()
+        {
+            SaveChanges();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Configurations.Add(new MeetingRoomConfiguration());
+        }
+    }
+}
